c07855140e5b1d066c113d425ace0f7349819168
Brendan C. Ward <bcward@astutespruce.com>
2022-02-11 21:10:25 -0800

    Add docstrings

--- src/_pymgl.cpp
+++ src/_pymgl.cpp
@@ -23,7 +23,30 @@ PYBIND11_MODULE(_pymgl, m) {
                       const std::optional<double> &,
                       const std::optional<std::string> &,
                       const std::optional<std::string> &>(),
-             "Class that provides static rendering using MapLibre GL native",
+             R"pbdoc(
+            Create Maplibre GL map instance.
+
+            Parameters
+            ----------
+            style : str
+                Mapbox GL style object as json-encoded string.
+            width : int, optional (default: 256)
+                Width of output map.
+            height : int, optional (default: 256)
+                Height of output map.
+            ratio : float, optional (default: 1)
+                Pixel ratio of output map.
+            longitude : float, optional (default: 0)
+                Map center longitude.
+            latitude : float, optional (default: 0)
+                Map center latitude.
+            zoom : float, optional (default 0)
+                Map zoom level, between 0 and 24.
+            token : str, optional
+                Token, if required for provider.
+            provider : str, one of {'mapbox', 'maptiler', 'maplibre', None}
+                Map resource provider, if required for sources listed in the style.
+        )pbdoc",
              py::arg("style"),
              py::arg("width")     = 256,
              py::arg("height")    = 256,
@@ -45,7 +68,36 @@ PYBIND11_MODULE(_pymgl, m) {
                  os << "pymgl." << self;
                  return os.str();
              })
-        .def("addImage", &Map::addImage, "add an image to the map")
+        .def("addImage",
+             &Map::addImage,
+             R"pbdoc(
+            Add an image to the map to be used as an icon or pattern.
+
+            Parameters
+            ----------
+            name : str
+                Name or ID of the image.  Used when referencing
+                this image in the style.
+            image : bytes
+                Raw pixels of image data.  PNG and other images must
+                be decoded to raw pixel values.
+            width : int
+                Width of image.
+            height : int
+                Height of image.
+            ratio : float
+                Pixel ratio of image to use when rendering the image.
+            make_sdf : bool
+                If True, image will be converted to an SDF format.  See
+                the Mapbox Style Specification for more information about
+                SDF images.
+        )pbdoc",
+             py::arg("name"),
+             py::arg("image"),
+             py::arg("width"),
+             py::arg("height"),
+             py::arg("ratio"),
+             py::arg("make_sdf"))
         .def_property_readonly("bearing", &Map::getBearing)
         .def_property_readonly("center", &Map::getCenter)
         .def_property_readonly("pitch", &Map::getPitch)
@@ -53,11 +105,11 @@ PYBIND11_MODULE(_pymgl, m) {
         .def_property_readonly("zoom", &Map::getZoom)
         .def(
             "renderPNG",
-            [](Map &self) -> py::bytes {
-                return py::bytes(self.renderPNG());
-            },
+            [](Map &self) -> py::bytes { return py::bytes(self.renderPNG()); },
             py::call_guard<py::gil_scoped_release>(),
-            "Render the map to PNG data")
+            R"pbdoc(
+                Render the map to PNG bytes.
+            )pbdoc")
         .def(
             "renderBuffer",
             [](Map &self) -> py::array_t<uint8_t> {
@@ -88,12 +140,33 @@ PYBIND11_MODULE(_pymgl, m) {
                 auto arr = py::array(size.first * size.second * 4, buf, capsule);
                 return arr;
             },
-            //   py::call_guard<py::gil_scoped_release>(),
-            "Render the map to a uint8 numpy array")
-        .def("setBearing", &Map::setBearing, "Set the bearing of the map")
+            R"pbdoc(
+                Render the map to an array of uint8 pixel values.
+            )pbdoc")
+        .def("setBearing",
+             &Map::setBearing,
+             R"pbdoc(
+                Set the bearing of the map.
+
+                Parameters
+                ----------
+                bearing : float
+                    Map bearing in degrees, between 0 and 360.
+            )pbdoc",
+             py::arg("bearing"))
         .def("setBounds",
              &Map::setBounds,
-             "Fit the map to the bounds, given an optional inset padding in pixels",
+             R"pbdoc(
+                Fit the map to the bounds, given an optional inset padding in pixels.
+
+                Parameters
+                ----------
+                xmin : float
+                ymin : float
+                xmax : float
+                ymax : float
+                padding : int, optional (default: 0)
+            )pbdoc",
              py::arg("xmin"),
              py::arg("ymin"),
              py::arg("xmax"),
@@ -101,14 +174,48 @@ PYBIND11_MODULE(_pymgl, m) {
              py::arg("padding") = 0)
         .def("setCenter",
              &Map::setCenter,
-             "Set the center longitude and latitude of the map",
+             R"pbdoc(
+                Set the center of the map.
+
+                Parameters
+                ----------
+                longitude : float
+                latitude : float
+            )pbdoc",
              py::arg("longitude"),
              py::arg("latitude"))
-        .def("setPitch", &Map::setPitch, "Set the pitch of the map")
-        .def("setZoom", &Map::setZoom, "Set the zoom of the map")
+        .def("setPitch",
+             &Map::setPitch,
+             R"pbdoc(
+                Set the pitch of the map.
+
+                Parameters
+                ----------
+                pitch : float
+                    Map pitch in degrees, between 0 and 85.
+            )pbdoc",
+             py::arg("pitch"))
+        .def("setZoom",
+             &Map::setZoom,
+             R"pbdoc(
+                Set the zoom level of the map.
+
+                Parameters
+                ----------
+                zoom : float
+                    Map zoom between 0 and 24
+            )pbdoc",
+             py::arg("zoom"))
         .def("setSize",
              &Map::setSize,
-             "Set the width and height of the map",
+             R"pbdoc(
+                Set the width and height of the map.
+
+                Parameters
+                ----------
+                width : int
+                height : int
+            )pbdoc",
              py::arg("width"),
              py::arg("height"));
 }
