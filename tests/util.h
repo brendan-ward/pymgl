1dbe3b03e3abb6958d42dd046b78670abc9bb6fa
Brendan C. Ward <bcward@astutespruce.com>
2022-03-11 20:18:10 -0800

    Allow Mapbox token to be optional in C++ tests

--- tests/util.h
+++ tests/util.h
@@ -12,7 +12,7 @@ namespace fs = std::filesystem;
 
 const string FIXTURES_PATH = fs::absolute("tests/fixtures/").string();
 
-const string get_token();
+const string get_token(bool required);
 mbgl::PremultipliedImage read_image(const string &filename);
 const string read_style(const string &filename);
 void write_test_image(const string &img, const string &filename, bool is_expected);
